3763160aa77ea47dce516572bbccdce2673f1617
Alessandro Leyva <josealeyva87@gmail.com>
2017-07-12 20:09:59 -0500

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+## iTalk-App
+
+iTalk is a full-stack mobile application that provides the user with common phrases in Spanish and English, the application would allow the user to speak and learn the basics of the desired language. It gives the user the possibility to use these common phrases, add phrases to your favorites, and check what new phrases other users have upload to the application. Also, it gives you the translation of any new phrase you enter, in Spanish or in English.
---
+++ server.js
@@ -0,0 +1,22 @@
+// requires
+var express = require('express');
+var app = express();
+var index = require('./modules/routes/index');
+// var register = require('./modules/routes/register');
+// var login = require('./modules/routes/login');
+// var italk = require('./modules/routes/italk');
+
+// uses
+app.use(express.static('public'));
+app.use('/', index);
+// app.use('/register', register);
+// app.use('/login', login);
+// app.use('/italk', italk);
+
+// globals
+var port = process.env.PORT || 2017;
+
+// spin up server
+app.listen(port, function(){
+  console.log('server up on:', port);
+}); // end spin up
